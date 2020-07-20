20a47d61312c1e54959ea8b603e42d8a05084d7d
carbine <92696852+carbine@users.noreply.github.com>
2020-07-20 20:26:43 +0000

    Added travis badge to readme

--- src/CarImpl.java
+++ src/CarImpl.java
@@ -21,4 +21,4 @@ public class CarImpl implements Car {
     private int velocity;
     
     public CarImpl(Road r, int lane, int v0) {
-        this.road = new RoadImpl(r
+        this.road = new RoadImpl(r.lanes(), r.leng
