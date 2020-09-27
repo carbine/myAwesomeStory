aeae971a7661e2122507f99a0e6f5dee7c04d10b
carbine <92696852+carbine@users.noreply.github.com>
2020-09-27 11:52:23 +0000

    Remove debug cruft.

--- src/IntersectImpl.java
+++ src/IntersectImpl.java
@@ -17,4 +17,4 @@ public class IntersectImpl implements Intersect {
     private List<Integer> delay;
     private final int maxDelay = 3;    
 
-    public IntersectImpl(List<Road> in,
+    public IntersectImpl(List<Road> in, List<Road> o
