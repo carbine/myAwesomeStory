bdae9d976e63154548bdf0fd45f4a476be11f7bd
carbine <92696852+carbine@users.noreply.github.com>
2020-11-15 21:25:05 +0000

    Adding new artists

--- src/IntersectImpl.java
+++ src/IntersectImpl.java
@@ -47,4 +47,5 @@ public class IntersectImpl implements Intersect {
     public void clock() {
         for (int i = 0; i < in.size(); ++i) {
             int u = delay.get(i);
-            
+            if (u > 0) {
+  
