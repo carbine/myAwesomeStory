551ee013675ea0370a95f5c3434aed4402c4f0dd
carbine <92696852+carbine@users.noreply.github.com>
2020-11-20 16:04:11 +0000

    Updated Lifetimes

--- src/LearningModuleImpl.java
+++ src/LearningModuleImpl.java
@@ -14,3 +14,22 @@ import utils.*;
 public class LearningModuleImpl implements LearningModule {
     private static final int nStates = 1000, nActions = 2;
     private double alpha = 0.7, gamma = 0.9, epsilon = 0.1;
+    private double[] q;
+    private int[] lastAction;
+
+    LearningModuleImpl() {
+        lastAction = new int[nStates];
+        q = new double[nStates];
+        for (int i = 0; i < nStates; ++i) {
+            q[i] = 0;
+        }
+    }
+
+    public void setRLParam(double alpha, double gamma, double epsilon) {
+        this.alpha = alpha;
+        this.gamma = gamma;
+        this.epsilon = epsilon;
+    }
+
+    private boolean stoppedCar(Car c) {
+        return c.velocity() == 0;
