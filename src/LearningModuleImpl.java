bbccbc9aa53786327076c8520a3bc0e79d3570eb
carbine <92696852+carbine@users.noreply.github.com>
2020-11-21 16:22:39 +0000

    Working on README

--- src/LearningModuleImpl.java
+++ src/LearningModuleImpl.java
@@ -33,3 +33,30 @@ public class LearningModuleImpl implements LearningModule {
 
     private boolean stoppedCar(Car c) {
         return c.velocity() == 0;
+    }
+
+    public int decide(int s) {
+        int bestAction = 0;
+        double bestQ = -100;
+        for (int i = 0; i < nActions; ++i) {
+            if (q[10*s + i] > bestQ) {
+                bestQ = q[10*s + i];
+                bestAction = i;
+            }
+        }
+        lastAction[s] = bestAction;
+        return bestAction;
+    }
+
+    public void learn(int a, int s, int sp) {
+        double newQ = (1-alpha) * q[s*nActions + a] +
+            alpha * (reward(sp) + gamma*maxQ(sp));
+        q[s*nActions + a] = newQ;
+    }
+    
+    public int stateCode(Intersect l, RoadMap m) {
+        int ret = l.lightSetting();
+    	for (int i = 0; i < l.in().size(); ++i) {
+    		Road r = l.in().get(i);
+    		int closestCar = 9;
+    		for (Car c : m.carsOn(i)) {
