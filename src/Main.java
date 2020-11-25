15e79c142367658b821c73f3497522164a015366
carbine <92696852+carbine@users.noreply.github.com>
2020-11-25 15:15:55 +0000

    version 1.5.1

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,16 @@
 COMP9417 Machine Learning
 Major Project - Traffic Lights Reinforcement Learning
 Beth Crane
+Gill Morris
+Nathan Wilson
+ */
+
+import interfaces.Car;
+import interfaces.LearningModule;
+import interfaces.RoadMap;
+import interfaces.Road;
+import interfaces.Intersect;
+import utils.Coords;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
