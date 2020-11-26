16512f7a1bc51922afd844841b88b7f8d0ff7731
carbine <92696852+carbine@users.noreply.github.com>
2020-11-26 11:35:35 +0000

    [RELEASE] merging 'master' into 'develop'

--- src/Main.java
+++ src/Main.java
@@ -16,3 +16,16 @@ import utils.Coords;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+public class Main {
+    public static void main (String[] args) {
+        //Graphics and runtime parameters
+        int runTime = 50200;
+        int quietTime = 50000;
+        boolean graphicalOutput = true;
+        boolean consoleOutput = false;
+        boolean output = graphicalOutput || consoleOutput;
+        double trafficIntensity = 0.2;
+
+        RoadMap map = new RoadMapImpl();
+        LearningModule learner = new LearningModuleImpl();
