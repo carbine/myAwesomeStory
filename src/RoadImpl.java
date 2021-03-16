70a93d70621303fc15e96c2b9d96e6b01286cbb0
carbine <92696852+carbine@users.noreply.github.com>
2021-03-16 15:53:50 +0000

    tipc: fix info leaks via msg_name in recv_msg/recv_stream

--- src/RoadImpl.java
+++ src/RoadImpl.java
@@ -4,3 +4,15 @@ COMP9417 Machine Learning
 Major Project - Traffic Lights Reinforcement Learning
 Beth Crane
 Gill Morris
+Nathan Wilson
+*/
+
+import interfaces.Road;
+
+public class RoadImpl implements Road {
+    private final int lanes, length;
+
+    public RoadImpl(int lanes, int length) {
+        this.lanes = lanes;
+        this.length = length;
+    }
