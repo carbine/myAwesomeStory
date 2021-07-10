ca2afd7e7f79f769d82984ce24ca8a859274982e
carbine <92696852+carbine@users.noreply.github.com>
2021-07-10 15:28:09 +0000

    Updated to latest

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -22,4 +22,5 @@ public class RoadMapImpl implements RoadMap {
     private Road[] roads;
     private List<Intersect> intersections;
     private List<Car>[] carsOn;
-    private LinkedList<Coords> intersectCoords = new LinkedList<Coords
+    private LinkedList<Coords> intersectCoords = new LinkedList<Coords>();
+    //priva
