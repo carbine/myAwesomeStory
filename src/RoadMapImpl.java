590b38c98725e7b508a09d7a60eaf0ac5cd3430f
carbine <92696852+carbine@users.noreply.github.com>
2021-08-15 13:22:53 +0000

    Update traffic.

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -23,4 +23,4 @@ public class RoadMapImpl implements RoadMap {
     private List<Intersect> intersections;
     private List<Car>[] carsOn;
     private LinkedList<Coords> intersectCoords = new LinkedList<Coords>();
-    //private HashMap<Road, Coords[]> roadLocations = new HashMap<Ro
+    //private HashMap<Road, Coords[]> roadLocations = new HashMap<Road, Coords[]>(
