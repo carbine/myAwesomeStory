09bf10b5ce70ed69ea4bb2710f99b16df40a050e
carbine <92696852+carbine@users.noreply.github.com>
2021-09-09 09:34:02 +0000

    FIX Linter errors

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -32,4 +32,4 @@ public class RoadMapImpl implements RoadMap {
     	{new Coords(23, 20), new Coords(42, 20)}, 
         {new Coords(42, 22), new Coords(23, 22)}, 
         {new Coords(43, 19), new Coords(43, 0)}, 
-   
+        {new Coord
