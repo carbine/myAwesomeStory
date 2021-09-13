8adf5a607fd21f7637cca0f1c389d1585f75a451
carbine <92696852+carbine@users.noreply.github.com>
2021-09-13 09:12:08 +0000

    update comments.

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -32,4 +32,5 @@ public class RoadMapImpl implements RoadMap {
     	{new Coords(23, 20), new Coords(42, 20)}, 
         {new Coords(42, 22), new Coords(23, 22)}, 
         {new Coords(43, 19), new Coords(43, 0)}, 
-        {new Coords(45, 0), new Coords(45, 19)}
+        {new Coords(45, 0), new Coords(45, 19)},
+        {new 
