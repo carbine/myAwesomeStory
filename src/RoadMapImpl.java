3d521cbcc5565cbaa6c5958f200d4287be2573d6
carbine <92696852+carbine@users.noreply.github.com>
2021-10-08 19:24:28 +0000

    Updated all to latest; excluded source .ts in npm

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -40,3 +40,4 @@ public class RoadMapImpl implements RoadMap {
         {new Coords(22, 23), new Coords(22, 42)}, 
         {new Coords(20, 42), new Coords(20, 23)}, 
         {new Coords(0, 43), new Coords(19, 43)}, 
+        {new Coords(
