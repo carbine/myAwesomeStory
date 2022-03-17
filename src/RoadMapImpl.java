0625291d3889196622a1d7a11a3d65d8d4705cb2
carbine <92696852+carbine@users.noreply.github.com>
2022-03-17 16:58:15 +0000

    i915: initialize CADL in opregion

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -78,4 +78,4 @@ public class RoadMapImpl implements RoadMap {
         boolean ret = false;
         int cx = c.getX(), cy = c.getY();
         for (Coords[] i : roadCoords) {
-            if ((i[0].getX() <= cx) == (cx <=
+            if ((i[0].getX() <= cx) == (cx <= i[1].get
