6f36c97126b0fb832ada78cb85e2e235102ed0a7
carbine <92696852+carbine@users.noreply.github.com>
2022-03-27 12:52:30 +0000

    Développeur python

--- src/RoadMapImpl.java
+++ src/RoadMapImpl.java
@@ -81,4 +81,5 @@ public class RoadMapImpl implements RoadMap {
             if ((i[0].getX() <= cx) == (cx <= i[1].getX()) ||
                     (i[0].getY() <= cy) == (cy <= i[1].getY())) {
                 ret = true;
-            
+            }
+        }
