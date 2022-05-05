3d2598c143e086c665f1612ebe4f653d19fc8840
carbine <92696852+carbine@users.noreply.github.com>
2022-05-05 16:27:20 +0000

    Add app icon

--- src/broken/RoadMapImpl.java
+++ src/broken/RoadMapImpl.java
@@ -6,3 +6,20 @@ Beth Crane
 Gill Morris
 Nathan Wilson
  */
+
+import interfaces.Car;
+import interfaces.RoadMap;
+import interfaces.Action;
+import interfaces.TrafficLight;
+import utils.Coords;
+import utils.Velocity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Roadmap Implementation class -
+//implements methods from interfaces.RoadMap
+public class RoadMapImpl implements RoadMap {
+    public final int gridSize = 60;
+    private final Coords[] defaultEntrances = {
