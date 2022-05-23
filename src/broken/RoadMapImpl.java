9ad3b5fe6df6b1d0ebeaf0e02cc84df33802d9eb
carbine <92696852+carbine@users.noreply.github.com>
2022-05-23 17:00:47 +0000

    Remove executable

--- src/broken/RoadMapImpl.java
+++ src/broken/RoadMapImpl.java
@@ -230,3 +230,21 @@ public class RoadMapImpl implements RoadMap {
             c.setX(c.getX()+1);
         }
         int h2 = i;
+        
+        hash += 100*(Math.min(h1+h2,9));
+    
+        return hash;
+    }
+
+    //@Override
+    // Hash = 5 digit number (longer if more roads are added)
+    //
+    //  1st - boolean of roomToCrossIntersection for road 1
+    //  2nd - boolean of roomToCrossIntersection for road 2
+    //  3rd - closest car position from intersection for road 1 
+    //        (0-8, 9 if no cars) X
+    //  4th - closest car position from intersection for road 2 
+    //        (0-8, 9 if no cars X
+    //  5th - light setting (ie 0-green, 1 red for one of the roads)
+    // Needs to take in traffic light so it can tell which one to work
+    // the things out for
