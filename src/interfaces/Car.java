2d96e42211d77720bfed003d5cace9cb6f3ff24f
carbine <92696852+carbine@users.noreply.github.com>
2022-06-29 05:52:39 +0000

    updated classpath

--- src/interfaces/Car.java
+++ src/interfaces/Car.java
@@ -6,3 +6,21 @@ Beth Crane
 Gill Morris
 Nathan Wilson
 */
+
+package interfaces;
+
+//import Road;
+
+public interface Car {
+
+    // tells the car to continue its journey. it should make up its
+    // own mind where its going, whether changing lanes is a good idea,
+    // etc.
+    Road road();
+
+    int distAlongRoad();
+
+    void move();
+
+    int velocity();
+}
