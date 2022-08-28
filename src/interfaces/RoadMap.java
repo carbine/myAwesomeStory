230a4c47561a539ac95b1ccc32b92e0abf68bf70
carbine <92696852+carbine@users.noreply.github.com>
2022-08-28 07:31:50 +0000

    Fix missing parenthesis

--- src/interfaces/RoadMap.java
+++ src/interfaces/RoadMap.java
@@ -4,3 +4,21 @@ package interfaces;
 COMP9417 Machine Learning
 Major Project - Traffic Lights Reinforcement Learning
 Beth Crane
+Gill Morris
+Nathan Wilson
+*/
+
+import interfaces.Road;
+import interfaces.Car;
+import interfaces.Intersect;
+import utils.Coords;
+import java.util.List;
+
+//Roadmap Interface - contains a representation of the road and intersections
+public interface RoadMap {
+
+    List<Car> carsOn(int i);
+
+    List<Intersect> intersections();
+
+    int nRoads();
