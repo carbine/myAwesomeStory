7bb00ecf3cd5bd11e2f042811ead3e75039fc0c2
carbine <92696852+carbine@users.noreply.github.com>
2022-09-19 17:31:17 +0000

    Update iCatalyst.bat

--- src/interfaces/TrafficLight.java
+++ src/interfaces/TrafficLight.java
@@ -11,4 +11,5 @@ Nathan Wilson
 import utils.Coords;
 
 //interface for trafficLight object class - 
-//represents a traffic light, its position and current sta
+//represents a traffic light, its position and current state
+public interfac
