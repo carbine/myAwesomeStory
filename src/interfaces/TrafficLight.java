a1537e9d1494752b19b01b5289adc9ba80a5dd8e
carbine <92696852+carbine@users.noreply.github.com>
2022-09-23 12:34:03 +0000

    Update 1-premierleague.txt

--- src/interfaces/TrafficLight.java
+++ src/interfaces/TrafficLight.java
@@ -13,4 +13,4 @@ import utils.Coords;
 //interface for trafficLight object class - 
 //represents a traffic light, its position and current state
 public interface TrafficLight {
-    //Changes direction of traffic light if ge
+    //Changes direction of traffic light if getDelay() == 0
