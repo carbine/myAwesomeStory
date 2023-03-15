1112f0cddacdff68959825d23eb5a26e60e76187
carbine <92696852+carbine@users.noreply.github.com>
2023-03-15 17:03:20 +0000

    Run update-remote.sh on cassandra

--- src/utils/Coords.java
+++ src/utils/Coords.java
@@ -63,4 +63,4 @@ public class Coords extends Object {
         if (obj == null)
             return false;
         Coords coords = (Coords) obj;
-        return (this.x == coords.getX() && th
+        return (this.x == coords.getX() && this.y == coords
