39b4af02998d7ab2165b667ea299a7d98f4ddd02
carbine <92696852+carbine@users.noreply.github.com>
2023-04-04 15:38:00 +0000

    updated tests

--- src/utils/Velocity.java
+++ src/utils/Velocity.java
@@ -6,3 +6,15 @@ COMP9417 Machine Learning
 Major Project - Traffic Lights Reinforcement Learning
 Beth Crane
 Gill Morris
+Nathan Wilson
+ */
+
+//Utility class to represent the x and y velocities of an object.
+public class Velocity {
+    private int x;
+    private int y;
+
+    public Velocity(int xSpeed, int ySpeed) {
+        this.x = xSpeed;
+        this.y = ySpeed;
+    }
